99d4d59781cdbc632814c89d9dbc73620a37aae7
FaridAliyev <feliyev636@gmail.com>
2024-07-10 11:07:58 +0400

    exaples

---
+++ While loop/app.js
@@ -0,0 +1,60 @@
+// Task 1 (1-10 kimi ədədlərin yazdırıması)
+
+let index = 0;
+while(index<=10){
+    console.log(index);
+    index++;
+}
+
+
+
+
+// Task 2 (Bir ədədi sıfıra qədər azaldan və hər addımı konsolda göstərən bir while döngüsü yazın.)
+
+// let number = 50;
+// while (number >= 0) {
+//     console.log(number);
+//     number--;
+// }
+
+
+
+
+// Task 3 (Bir arrayin bütün elementlərini yazdırmaq üçün while döngüsü istifadə edin.)
+
+// let array = [23, 32, 56, 32, 58, 79];
+// let arrayIndex = 0
+// while (arrayIndex < array.length) {
+//     console.log(array[arrayIndex]);
+//     arrayIndex++;
+// }
+
+
+
+
+// Task 4 (Bir ədədin faktorialını hesablamaq üçün while döngüsü istifadə edin.)
+
+// let number = parseInt(prompt("BIr reqem daxil edin"));
+// let calculation = 1;
+// while (0 < number) {
+//     calculation = calculation * number;
+//     number--;
+// }
+// console.log(calculation);
+
+
+
+
+// Task 5 (Bir stringi tərsinə çevirən while döngüsü yazın.)
+
+// let string = prompt("Bir 'String' daxil edin");
+// let stringIndex = string.length-1;
+// let newString = "";
+// while (stringIndex >= 0) {
+//     newString += string[stringIndex];
+//     stringIndex--;
+// }
+// console.log(newString);
+
+
+

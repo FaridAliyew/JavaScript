8771aa404f53fd19f473f82df263ae7e41d20cb5
FaridAliyev <feliyev636@gmail.com>
2024-07-14 17:44:48 +0400

    Array Method

---
+++ Array Method/Pop method/script.js
@@ -0,0 +1,66 @@
+// Task 1 (Bir array yaradın və daxilində beş element olsun. Sonra pop metodunu istifadə edərək array-in son elementini silin və yeni array-i çap edin.)
+
+// const myArray = [12,"Hello",true,false,"45"];
+// let deletedEle = myArray.pop();
+// console.log(myArray);
+
+
+
+
+// Task 2 Başlanğıcda boş bir array yaradın. push metodunu istifadə edərək bir neçə element əlavə edin. Sonra pop metodunu istifadə edərək array-in son elementini silin və yeni array-i çap edin.
+
+// const array = [];
+// array.push(12, "Alex", null);
+// array.pop();
+// console.log(array); 
+
+
+
+
+// Task 3 (Bir arrayin son elementini götürüb digər array-ə əlavə etmək)
+
+// const arrayOne = ["Hello","wolrd","Alex"];
+// const arrayTwo = [12,32,54,21];
+
+// let lastDeletEl = arrayOne.pop();
+
+// arrayTwo.push(lastDeletEl);
+// console.log(arrayTwo);
+
+
+
+
+
+// Task 4 (Arrayin içərisinin tamamilə silinməsi)
+
+// const array = [12, 3, 46, 86, 2];
+// const length = array.length;
+
+// for (let index = 0; index < length; index++) {
+//     array.pop();
+// }
+
+// console.log(array);
+
+
+
+
+// Task 5 (Verilmiş arraydən isdənilən sayda value götürüb yeni bir array-ə əlavə edilməsi)
+
+// const person = ["Alex", "Simon", "James", "Melina", "Toby", "Tom"];
+// let newArray = [];
+
+// for (let index = 0; index < 3; index++) {
+//     let deleted = person.pop()
+//     newArray.push(deleted);
+// }
+// console.log(newArray);
+
+
+
+
+
+
+
+
+

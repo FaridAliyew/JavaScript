83a56bda87461a9dc1801a0c0ae15ff1c73409ff
FaridAliyev <feliyev636@gmail.com>
2024-07-17 12:19:52 +0400

    examples

---
+++ Spread operator/script.js
@@ -0,0 +1,33 @@
+// Task 1 - İki array-i birləşdirin:
+
+const arrayOne = [1,2,3,4];
+const arrayTwo = ["Hello","Wolrd","Alex"];
+const cobinedArray =[...arrayOne, ...arrayTwo];
+console.log(cobinedArray);
+
+
+
+
+// Task 2 - Verilən array-in elementlərini funksiya parametrləri kimi göndərin
+
+// const sum = (a, b, c) => a + b + c;
+
+// let array = [12, 45, 25];
+// const result = sum(...array);
+// console.log(result);
+
+
+
+
+// Task 3 - Nested array-i flatten edin (yəni, tək səviyyəli array-ə çevirin):
+
+// const nestedArray = [1, 4, [8, 9], [234,345]];
+// const result = [].concat(...nestedArray);
+// console.log(result)
+
+
+
+
+
+
+
